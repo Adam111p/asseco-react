7d56ed7463aa73214b53e28941214657b97eba80
Adam Popławski <adam.poplawski@asseco.pl>
2023-01-10 11:36:46 +0100

    tasdk003

--- apps/music-player-final/src/components/Playlist/Playlist.tsx
+++ apps/music-player-final/src/components/Playlist/Playlist.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { ISong } from '../../songs';
 import Player from '../Player/Player';
 import Song from '../Song/Song';
@@ -11,12 +11,16 @@ interface PlaylistProps {
 type SongState = { isPlaying: boolean, isFavorite: boolean };
 
 const Playlist: FC<PlaylistProps> = ({ title, songs }) => {
-  const [ songState, setSongState ] = useState<SongState>({
+  const [songState, setSongState] = useState<SongState>({
     isPlaying: false,
     isFavorite: false,
   });
 
-  const [ currentlyPlaying, setCurrentlyPlaying ] = useState<ISong | null>(null);
+  const [songsState, setSongsState] = useState(songs);
+
+  const [currentlyPlaying, setCurrentlyPlaying] = useState<ISong | null>(null);
+
+  const [sortState, setSorttate] = useState(1);
 
   const play = () => {
     setSongState((prevState) => ({
@@ -24,10 +28,29 @@ const Playlist: FC<PlaylistProps> = ({ title, songs }) => {
       isFavorite: songState.isFavorite,
     }));
   }
+  useEffect(() => {    
+      document.title = `Now you listen ${currentlyPlaying?.performer} `; 
+      // setCurrentlyPlaying(()=>{
+      //   if(currentlyPlaying?.counter) currentlyPlaying.counter+1;
+      //   return currentlyPlaying;
+      // });
+    },[currentlyPlaying]);
 
+  // useEffect(()=>
+  // {
+
+  // }
+  // );
+  const handleSort = () => {
+    const result = [...songs].sort((a, b) => a.duration - b.duration * sortState);
+    setSorttate(p => p * -1);
+    console.log(result);
+    setSongsState(result);
+  }
   return (
     <div onClick={play} style={{ border: '1px solid', color: '#fff', padding: 15, }}>
       <h2>{title}</h2>
+      <button onClick={handleSort}>{sortState > 0 ? "Sort ASC" : "Sort Desc"}</button>
       <Player
         title={currentlyPlaying ? currentlyPlaying.title : ''}
         duration={currentlyPlaying ? currentlyPlaying.duration : 0}
@@ -39,8 +62,8 @@ const Playlist: FC<PlaylistProps> = ({ title, songs }) => {
         margin: 0,
         padding: 0,
       }}>
-        {songs.map((song, i) => <Song
-          index={i + 1} 
+        {songsState.map((song, i) => <Song
+          index={i + 1}
           handleClick={setCurrentlyPlaying}
           {...song}
         />)}
